b5af40877f6ce35adf9c203874537a621eb0a81c
Austin Fiddes <austinfiddes@outlook.com>
2025-02-25 09:05:32 -0700

    Initial commit - Setup ASP.NET Core MVC project

--- MyFirstMVCApp/Program.cs
+++ MyFirstMVCApp/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Windows}/{action=Index}/{id?}");
 
 app.Run();
